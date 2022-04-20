3d9ff3049da0feb763aa79d750c579675c394676
wuxiao <xiaopig0106@gmail.com>
2022-04-20 17:20:53 +0800

    用户输入商品信息

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -1,3 +1,6 @@
+let names =''
+let price = ''
+
 Page({
 
   onLoad(){
@@ -11,10 +14,35 @@ Page({
   })
     .catch(err => {console.error('商品请求失败', err)})
   },
+  //跳转页面
   goDetail(e){
     console.log(e.currentTarget.dataset.id)
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + e.currentTarget.dataset.id,
     })
+  },
+  // 获取用户输入的商品名和价格
+  getName(e){
+    names = e.detail.value
+    console.log(names)
+  },
+  getPrice(e){
+    price = e.detail.value
+    console.log(price)
+  },
+  // 添加商品到数据库
+  addGoods(){
+    console.log('商品名：', names)
+    console.log('价格：', price)
+    if(names == '' || price == ''){
+      wx.showToast({
+        icon: 'none',
+        title: '商品名或价格不能为空'
+      })
+    }
+    // wx.cloud.database().collection('goods').add({
+    //   name: names,
+    //   price: price
+    // })
   }
 })
