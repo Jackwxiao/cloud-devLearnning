4d8b5346b6c7dd3f536188810459aed93c362f1d
wuxiao <xiaopig0106@gmail.com>
2022-04-20 15:29:24 +0800

    添加商品

---
+++ pages/add/add.js
@@ -0,0 +1,77 @@
+// pages/add/add.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+  },
+  // 添加商品
+  add() {
+    wx.cloud.database().collection('goods').add({
+      data:{  // 添加数据
+        name: '车厘子',
+        price: '112'
+      }
+    }).then(res => {
+      console.log('添加成功',res)
+    }).catch(err => {
+      console.log('添加失败',err)
+    })
+  },
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
