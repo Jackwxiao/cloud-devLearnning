db51602d41d890bf2caee23d606effd0ceeaac7f
wuxiao <xiaopig0106@gmail.com>
2022-04-20 17:41:55 +0800

    输入的商品信息更新到页面和数据库

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -1,48 +1,59 @@
-let names =''
+let names = ''
 let price = ''
 
 Page({
 
-  onLoad(){
-    wx.cloud.database().collection('goods')
-    .get()
-    .then(res => {
-      console.log('商品请求成功', res)
-      this.setData({
-        list: res.data
-      })
-  })
-    .catch(err => {console.error('商品请求失败', err)})
+  onLoad() {
+    this.getList()
   },
+  // 获取列表数据
+  getList() {
+    wx.cloud.database().collection('goods')
+      .get()
+      .then(res => {
+        console.log('商品请求成功', res)
+        this.setData({
+          list: res.data
+        })
+      }).catch(err => { console.error('商品请求失败', err) })
+  },
+
   //跳转页面
-  goDetail(e){
+  goDetail(e) {
     console.log(e.currentTarget.dataset.id)
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + e.currentTarget.dataset.id,
     })
   },
   // 获取用户输入的商品名和价格
-  getName(e){
+  getName(e) {
     names = e.detail.value
-    console.log(names)
   },
-  getPrice(e){
+  getPrice(e) {
     price = e.detail.value
-    console.log(price)
   },
   // 添加商品到数据库
-  addGoods(){
-    console.log('商品名：', names)
-    console.log('价格：', price)
-    if(names == '' || price == ''){
+  addGoods() {
+    if (names == '' || price == '') {
       wx.showToast({
         icon: 'none',
         title: '商品名或价格不能为空'
       })
     }
-    // wx.cloud.database().collection('goods').add({
-    //   name: names,
-    //   price: price
-    // })
+    else {
+      // 更新数据库
+      wx.cloud.database().collection('goods').add({
+        data: {
+          name: names,
+          price: price
+        }
+      }).then(res => {
+        console.log('添加商品成功', res)
+        this.getList()
+      }).catch(err => {
+        console.error('添加商品失败', err)
+      })
+    }
+
   }
 })
