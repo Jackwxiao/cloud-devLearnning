118a37c50a0fa2eca55b4d5a9569946749a4a8d4
wuxiao <xiaopig0106@gmail.com>
2022-04-20 15:12:01 +0800

    doc单条查询

--- pages/shujuku/shujuku.js
+++ pages/shujuku/shujuku.js
@@ -1,15 +1,13 @@
 // pages/shujuku/shujuku.js
 Page({
   data:{
-    list:[]
+    list: [],
+    good: {}
   },
   onLoad(){
     const db = wx.cloud.database()
-    // promise写法。where条件查询
-    db.collection('goods').where({
-      name: "苹果"
-    })
-    .get()
+    // promise写法。where条件查询。doc查询单条数据
+    db.collection('goods').get()
     .then(res => {
       console.log(res)
       this.setData({
@@ -18,6 +16,15 @@ Page({
     })
     .catch(err => {
       console.log(err)
+    })
+    db.collection('goods').doc('f1dedd58625f9f7500aec6475187f10f').get()
+    .then(res => {
+      this.setData({
+        good: res.data 
+      })
+    })
+    .catch(err => {
+      console.log('请求失败')
     })
   }
 })
